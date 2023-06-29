6dcd1f51d9970c002d5077e998cb06ed12f3d80e
Fgr7 <55815847+Fgr7@users.noreply.github.com>
2023-06-29 15:37:10 -0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CounterModule } from './counter/components/counter.module';
 import { HeroesModule } from './Heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, CounterModule, HeroesModule],
+  imports: [BrowserModule, FormsModule, CounterModule, HeroesModule, DbzModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
